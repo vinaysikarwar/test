9143fb1681a39f790f4f795ac621fb83473d95a4
Vinay Sikarwar <svinay.1987@gmail.com>
2014-09-26 13:48:34 +0530

    Now i am deleting all the files

--- app/code/local/WTC/ButtonsColor/Block/Index.php
+++
@@ -1,8 +0,0 @@
-<?php   
-class WTC_ButtonsColor_Block_Index extends Mage_Core_Block_Template{   
-
-
-
-
-
-}
--- app/code/local/WTC/ButtonsColor/Helper/Data.php
+++
@@ -1,5 +0,0 @@
-<?php
-class WTC_ButtonsColor_Helper_Data extends Mage_Core_Helper_Abstract
-{
-}
-	 
